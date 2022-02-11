ddaf9e47a273ed5628bf2909b1263bd3f0652c92
ahiou <ahiou1ahiou@gmail.com>
2022-02-11 21:04:16 +0900

    FEAT: Installed Mongoose and connected to MongoDB successfully.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,6 +20,12 @@ if(process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 
-app.listen(PORT, () => {
-  console.log(`server running in [${process.env.NODE_ENV} mode] on port ${PORT}`)
-})
+const server = app.listen(PORT, () => {
+  console.log(`server running in [${process.env.NODE_ENV} mode] on port ${PORT}`.yellow)
+});
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server % Exit process
+  server.close(() => process.exit(1));
+});
