2259765228931a00b13e77273a35f44ab8547f79
ahiou <ahiou1ahiou@gmail.com>
2022-02-13 16:41:18 +0900

    FEAT: Routes for courses are all set.

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,10 +1,11 @@
 const asyncHandler = require('../middleware/async');
 const Course = require('../models/course');
+const ErrorResponse = require('../utils/errorResponse');
 
 /**
  * @description Get all courses from db (that matches the query)
- * @route       /api/v1/courses
- * @route       /api/v1/bootcamps/:bootcampId/courses
+ * @route       GET /api/v1/courses
+ * @route       GET /api/v1/bootcamps/:bootcampId/courses
  * @access      Public
  */
 exports.getCourses = asyncHandler(async (req, res, next) => {
@@ -23,4 +24,77 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     count: courses.length,
     data: courses,
   })
-})
+});
+
+/**
+ * @description Get a course with matching ID
+ * @route       GET /api/v1/courses
+ * @access      Public
+ */
+exports.getSingleCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if(!course) {
+    return next(new ErrorResponse(`Course not found with ID of ${req.params.id}`, 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+/**
+ * @description Save new course to DB 
+ * @route       POST /api/v1/courses
+ * @access      Private
+ */
+exports.createCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: course,
+  })
+});
+
+/**
+ * @description Update a course with new data
+ * @route       PUT /api/v1/courses/:id
+ * @access      Private
+ */
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!course) {
+    return next(new ErrorResponse(`Course not found with ID of ${req.params.id}`, 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+/**
+ * @description Delete a course
+ * @route       DELETE /api/v1/courses/:id
+ * @access      Private
+ */
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(new ErrorResponse(`Course not found with ID of ${req.params.id}`, 404));
+  }
+
+  await course.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const { getCourses } = require('../controllers/courses');
+const { getCourses, getSingleCourse, createCourse, updateCourse, deleteCourse } = require('../controllers/courses');
 const router = express.Router({ mergeParams: true });
 
 router.route('/')
-  .get(getCourses);
+  .get(getCourses)
+  .post(createCourse);
+
+router.route('/:id')
+  .get(getSingleCourse)
+  .put(updateCourse)
+  .delete(deleteCourse);
 
 module.exports = router;
 
